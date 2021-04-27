33e37405272d6c1b2f82021e7335c4b07fdfb59f
Kovács Endre <kovacsendre96@gmail.com>
2021-04-27 19:20:03 +0200

    email js

--- form-validation.js
+++ form-validation.js
@@ -64,6 +64,17 @@ function emailCheck(input) {
 
 
 // If everything is alright first feedback message than send the message to the email
+
+/* function sendMail(params){
+
+    var tempParams ={
+        név:username.value,
+        telefonszám:document.getElementById('telefonszám').value,
+        email:email.value,
+        üzenet:message.value,
+    }
+    emailjs.send('service_3ew9vtr','template_rzesgwf',tempParams)
+} */
 function send() {
 
     const feedback = document.querySelector('.feedback');
@@ -73,6 +84,8 @@ function send() {
     if (errorInput.length === 0) {
         feedback.classList = ('feedback succes');
         feedback.innerText = "Üzenet sikeresen elküldve.";
+        form.reset();
+     /*    sendMail(); */
     }
     else {
         feedback.classList = ('feedback error');
